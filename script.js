4b5a708bbc9c7b772a58308ff119253961ab4f02
KevinMolinuevo <132021342+KevinMolinuevo@users.noreply.github.com>
2024-09-26 19:27:28 +0000

    fixed

---
+++ script.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const apiKey= process.env.API_KEY;
+
+function getWeather(){
+   
+    
+    const city = document.getElementById('city').value;
+
+    if (!city){
+        alert('Please enter a city');
+        return;
+    }
+    const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid${apiKey}`;
+    const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+
+    fetch(currentWeatherUrl)
+          .then(response=> response.json)
+          .then(data=> {
+            displayWeather(data);
+    })
+          .catch(error=>{
+            console.error('Error fetching current weather data:', error);
+            alert('Error fetching current weather data. Please try again.');
+    });
+
+    fetch(forecastUrl)
+          .then(response=>response.json())
+          .then(data=>{
+            displayHourlyForecast(data.list);
+          })
+          .catch(error=> {
+            console.error('Error fetching hourly forecast data', error);
+            alert('Error fetching hourly forecast data:, error');
+          });
+             
+}
